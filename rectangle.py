dc75c9311b06be0fc96ff44bc10b5d2e8d4aae57
man_amv <146768577+manamv@users.noreply.github.com>
2025-12-12 02:05:09 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,26 +1,3 @@
-# def area(a, b): 
-#     '''takes the modules of the perpendicular sides of the rectangle a (height), b (width), returns its area'''
-#     return a * b 
-
-# def perimeter(a, b): 
-#     '''takes the modules of different sides of rectangle a, b, returns its perimeter'''
-#     return 2 * (a + b)
-
-
-# import unittest
-
-# class RectangleTestCase(unittest.TestCase):
-#     def test_zero_mul(self):
-#         res = area(10, 0)
-#         self.assertEqual(res, 0)
-
-#     def test_square_mul(self):
-#         res = area(10, 10)
-#         self.assertEqual(res, 100)
-
-
-
-
 def area(a: float, b: float) -> float:
     """
     Calculate the area of a rectangle.
